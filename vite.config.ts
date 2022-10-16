c90a5216bbd1b653f71462c5d02291f2a263479d
lizhiquan <842048365@qq.com>
2022-10-17 00:10:48 +0800

    feat: add husky

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-      "hooks": path.resolve(__dirname, 'src/hooks'),
-      "store": path.resolve(__dirname, 'src/store'),
-      "views": path.resolve(__dirname, 'src/views'),
-      "components": path.resolve(__dirname, 'src/components'),
-      "utils": path.resolve(__dirname, 'src/utils'),
-      "service": path.resolve(__dirname, 'src/service'),
-    },
-  },
+      '@': path.resolve(__dirname, 'src'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      store: path.resolve(__dirname, 'src/store'),
+      views: path.resolve(__dirname, 'src/views'),
+      components: path.resolve(__dirname, 'src/components'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      service: path.resolve(__dirname, 'src/service')
+    }
+  }
 });
