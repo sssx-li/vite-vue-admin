09eebc6cfcc300a740d9b01634d2d26084a1c715
lizhiquan <842048365@qq.com>
2022-10-24 23:00:48 +0800

    feat: add element-plus

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { viteMockServe } from 'vite-plugin-mock';
 
-import UnpluginVueComponents from 'unplugin-vue-components/vite';
+import Components from 'unplugin-vue-components/vite';
 import Icons from 'unplugin-icons/vite';
 import IconsResolver from 'unplugin-icons/resolver';
 import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
 import AutoImport from 'unplugin-auto-import/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -26,11 +27,13 @@ export default defineConfig(({ command }) => {
         eslintrc: {
           enabled: true,
           filepath: './.eslintrc-auto-import.json'
-        }
+        },
+        resolvers: [ElementPlusResolver()]
       }),
-      UnpluginVueComponents({
+      Components({
         dts: false,
         resolvers: [
+          ElementPlusResolver(),
           IconsResolver({
             customCollections: ['sy']
           })
