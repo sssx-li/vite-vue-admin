0925c908cefce09ecd27f771e1d28f883c634b80
lizq <lizq@epshealth.com>
2022-11-11 14:55:05 +0800

    perf: 更新Unocss配置

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 import AutoImport from 'unplugin-auto-import/vite';
 
 import Unocss from 'unocss/vite';
-import { presetAttributify, presetUno } from 'unocss';
+import { presetAttributify, presetUno, presetIcons } from 'unocss';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
@@ -48,36 +48,8 @@ export default defineConfig(({ command }) => {
         }
       }),
       Unocss({
-        presets: [presetUno(), presetAttributify()],
-        rules: [
-          ['fl', { display: 'flex' }],
-          ['fl-center', { 'justify-content': 'center', 'align-items': 'center' }],
-          ['fl-column', { 'flex-direction': 'column' }],
-          [/^fl-(\d+)$/, ([, d]) => ({ flex: d })],
-          [/^flw-(\S+)$/, ([, str]) => ({ 'flex-wrap': str })],
-          [/^fla-(\S+)$/, ([, str]) => ({ 'align-items': str })],
-          ['tac', { 'text-align': 'center' }],
-          ['tar', { 'text-align': 'right' }],
-          ['tal', { 'text-align': 'left' }],
-          ['fw-bold', { 'font-weight': 'bold' }],
-          [/^c-(\S+)$/, ([, color]) => ({ color })],
-          [/^fw-(\d+)$/, ([, d]) => ({ 'font-weight': d })],
-          [/^h-(\d+)$/, ([, d]) => ({ height: `${d}px` })],
-          [/^w-(\d+)$/, ([, d]) => ({ width: `${d}px` })],
-          [/^mt-(\d+)$/, ([, d]) => ({ 'margin-top': `${d}px` })],
-          [/^mr-(\d+)$/, ([, d]) => ({ 'margin-right': `${d}px` })],
-          [/^mb-(\d+)$/, ([, d]) => ({ 'margin-bottom': `${d}px` })],
-          [/^ml-(\d+)$/, ([, d]) => ({ 'margin-left': `${d}px` })],
-          [/^p-(\d+)$/, ([, d]) => ({ padding: `${d}px` })],
-          [/^pt-(\d+)$/, ([, d]) => ({ 'padding-top': `${d}px` })],
-          [/^pr-(\d+)$/, ([, d]) => ({ 'padding-right': `${d}px` })],
-          [/^pb-(\d+)$/, ([, d]) => ({ 'padding-bottom': `${d}px` })],
-          [/^pl-(\d+)$/, ([, d]) => ({ 'padding-left': `${d}px` })],
-          [/^fz-(\d+)$/, ([, d]) => ({ 'font-size': `${d}px` })],
-          [/^lh-(\d+)$/, ([, d]) => ({ 'line-height': `${d}px` })],
-          [/^br-(\d+)$/, ([, d]) => ({ 'border-radius': `${d}px` })],
-          ['cursor', { cursor: 'pointer' }]
-        ]
+        presets: [presetUno(), presetAttributify(), presetIcons()],
+        rules: [['cursor', { cursor: 'pointer' }]]
       })
     ],
     resolve: {
