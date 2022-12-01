4112cb5e459f897fcc6da8417cf49c302813307f
lizhiquan <842048365@qq.com>
2022-12-01 10:38:52 +0800

    feat: 虚拟列表

--- src/views/main/config/index.vue
+++ src/views/main/config/index.vue
@@ -1,7 +1,49 @@
 <template>
-  <div class="config-container">config</div>
+  <div class="list" ref="listRef" @scroll.passive="getScroll($event)">
+    <div :style="{ height: 40 * options.length + 'px', width: '100%' }">
+      <div
+        class="item"
+        v-for="(item, index) in options.slice(min - 2, min + 10)"
+        :key="index"
+        :style="{ top: `${40 * item.label}px` }"
+      >
+        {{ item.value }}:{{ item.label }}
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts" name="config"></script>
+<script setup lang="ts" name="config">
+let min = ref(2);
+const getScroll = (event: any) => {
+  let scrollTop = event.target.scrollTop;
+  // 当滚动距离大于2条数据的高度时开始动态计算
+  if (scrollTop > 2 * 40) {
+    min.value = Math.ceil(scrollTop / 40);
+  } else {
+    min.value = 2;
+  }
+};
+const options = ref<Array<{ label: number; value: string }>>(
+  Array.from({ length: 100000 }).map((_, idx) => ({
+    value: `Option ${idx + 1}`,
+    label: idx
+  }))
+);
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.list {
+  margin: 10px auto;
+  width: 300px;
+  height: 200px;
+  overflow: auto;
+  position: relative;
+  .item {
+    height: 40px;
+    line-height: 40px;
+    position: absolute;
+    width: 100%;
+  }
+}
+</style>
