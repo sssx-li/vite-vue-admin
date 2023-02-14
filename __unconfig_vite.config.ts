91dd7880228739d87563ea6e0f7689cd23fe99ae
lizhiquan <842048365@qq.com>
2023-02-14 12:56:28 +0800

    fix: top await 问题修复

--- __unconfig_vite.config.ts
+++ __unconfig_vite.config.ts
@@ -1,3 +1,10 @@
+let __unconfig_data;
+let __unconfig_stub = function (data = {}) {
+  __unconfig_data = data;
+};
+__unconfig_stub.default = (data = {}) => {
+  __unconfig_data = data;
+};
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
@@ -11,11 +18,11 @@ import { FileSystemIconLoader } from 'unplugin-icons/loaders';
 
 import AutoImport from 'unplugin-auto-import/vite';
 
-import Unocss from 'unocss/vite';
+const Unocss = __unconfig_stub;
 import { presetAttributify, presetUno, presetIcons } from 'unocss';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+const __unconfig_default = defineConfig(({ command }) => {
   return {
     plugins: [
       vue(),
@@ -66,3 +73,7 @@ export default defineConfig(({ command }) => {
     }
   };
 });
+
+if (typeof __unconfig_default === 'function')
+  __unconfig_default(...[{ command: 'serve', mode: 'development' }]);
+export default __unconfig_data;
